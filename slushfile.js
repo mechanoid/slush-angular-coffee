f956e7f9e170a854246a5a4201f44e592b6eee1b
Falk Hoppe <falkhoppe81@gmail.com>
2014-04-26 14:10:12 +0200

    initial commit, added slushfile and package description

---
+++ slushfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp'),
+    install = require('gulp-install'),
+    conflict = require('gulp-conflict'),
+    template = require('gulp-template');
+
+gulp.task('default', function (done) {
+  
+});
